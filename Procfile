3c72fc877f4caaa07841417fe46f88a13b58e92e
humanauction <t.a.harris@gmail.com>
2025-07-21 15:02:05 +0100

    project setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn routine_cap.wsgi
---
+++ home/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoutineCapAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'routine_cap_app'
