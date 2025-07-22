d20d5b113aa3070e074c514b8d6fbf277a9d5f34
humanauction <t.a.harris@gmail.com>
2025-07-22 11:55:22 +0100

    add user authentication. add views signup, login, logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView
+from .forms import SignupForm, LoginForm
+from .services import register_user, send_welcome_email
 from django.shortcuts import render
 
 # Create your views here.
+class SignupView(FormView):
+    template_name = 'authentication/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('authentication:login')
+
+    def form_valid(self, form):
+        """"register new user"""
+        user = register_user(
+            username=form.cleaned_data['username'],
+            email=form.cleaned_data['email'],
+            password=form.cleaned_data['password1']
+        )
+        """send welcome email"""
+        send_welcome_email(user)
+        return super().form_valid(form)
+
+class LoginView(DjangoLoginView):
+    template_name = 'authentication/login.html'
+    authentication_form = LoginForm
+    redirect_authenticated_user = True
+
+
+class LogoutView(LogoutView):
+    next_page = reverse_lazy('authentication:login')
+
