6b885cd416e75dcd14e7cd17493e821d892ac5b1
humanauction <t.a.harris@gmail.com>
2025-07-23 17:54:09 +0100

    add: migrate CustomUser password field, update model definition

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 class CustomUser(AbstractUser):
     """
@@ -9,8 +9,20 @@ class CustomUser(AbstractUser):
     """
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
     created_on = models.DateTimeField(auto_now_add=True)
     pass
     class Meta:
         ordering = ["-date_joined"]
+
+
+def create_normal_user(username, email, password):
+    User = get_user_model()
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password,
+        is_staff=False,
+        is_superuser=False
+    )
+    return user
